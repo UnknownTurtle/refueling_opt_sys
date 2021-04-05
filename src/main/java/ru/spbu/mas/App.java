9c489c4a57fdb64b5a4f115cc33fc22e0607d6c9
Gregory <uhbifr@rambler.ru>
2021-04-06 00:09:16 +0300

    new ingonre 5

--- src/main/java/ru/spbu/mas/App.java
+++ src/main/java/ru/spbu/mas/App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) {
         MainController mc = new MainController();
         mc.initAgents();
-        //hello
+        //hello 2
     }
 }
